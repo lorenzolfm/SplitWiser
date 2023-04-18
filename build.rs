bfd0812bc62fb2f179ec9a398fd95993139c3330
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-04-18 20:16:36 -0300

    hello world!

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(false)
-        //.protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["proto/splitwiser.proto"], &["proto/"])
         .map_err(Into::into)
 }
