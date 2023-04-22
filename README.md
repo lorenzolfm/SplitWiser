db105058717705a6f33293a02de9f6cae50c2c37
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-04-22 10:34:56 -0300

    Adding todos

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SplitWiser
+
+# TODOs
+
+* Feature: Add expanse
+* Feature: Add revenue
+* Feature: Delete expanse
+* Feature: Delete revenue
+* Feature: Handle installments
+* Feature: Add expanse and specify the month it should be charged on
---
+++ db/src/queries/user_revenues.rs
@@ -0,0 +1,24 @@
+use diesel::{ExpressionMethods, PgConnection, QueryResult, RunQueryDsl};
+use schema::schema::user_revenues;
+use time::OffsetDateTime;
+
+pub struct CreateParams {
+    pub user_id: i32,
+    pub amount: i64,
+    pub description: String, // TODO: Use &'a str
+    pub incoming_at: OffsetDateTime,
+    pub created_at: OffsetDateTime,
+}
+
+pub fn create(conn: &mut PgConnection, p: CreateParams) -> QueryResult<i32> {
+    diesel::insert_into(user_revenues::table)
+        .values((
+            user_revenues::user_id.eq(p.user_id),
+            user_revenues::amount.eq(p.amount),
+            user_revenues::incoming_at.eq(p.incoming_at),
+            user_revenues::description.eq(p.description),
+            user_revenues::created_at.eq(p.created_at),
+        ))
+        .returning(user_revenues::id)
+        .get_result(conn)
+}
