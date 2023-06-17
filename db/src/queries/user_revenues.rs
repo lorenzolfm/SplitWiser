39ee716128691fa97ed6dc1bad03be3ac195f84f
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-06-17 15:43:01 -0300

    DB: Summing user revenues

--- db/src/queries/user_revenues.rs
+++ db/src/queries/user_revenues.rs
@@ -1,4 +1,5 @@
-use diesel::{ExpressionMethods, PgConnection, QueryResult, RunQueryDsl};
+use bigdecimal::BigDecimal;
+use diesel::{ExpressionMethods, PgConnection, QueryDsl, QueryResult, RunQueryDsl};
 use schema::schema::user_revenues;
 use time::OffsetDateTime;
 
@@ -30,6 +31,20 @@ pub fn delete(conn: &mut PgConnection, id: i32, user_id: i32) -> QueryResult<Use
         .filter(user_revenues::id.eq(id))
         .filter(user_revenues::user_id.eq(user_id))
         .returning(user_revenues::id)
+        .get_result(conn)
+}
+
+pub fn revenue_for_period(
+    conn: &mut PgConnection,
+    user_id: i32,
+    from: OffsetDateTime,
+    until: OffsetDateTime,
+) -> QueryResult<Option<BigDecimal>> {
+    user_revenues::table
+        .filter(user_revenues::user_id.eq(user_id))
+        .filter(user_revenues::incoming_at.gt(from))
+        .filter(user_revenues::incoming_at.le(until))
+        .select(diesel::dsl::sum(user_revenues::amount_cents))
         .get_result(conn)
 }
 
@@ -74,6 +89,51 @@ mod test {
 
             let res = test_with_amount(1);
             assert!(res.is_ok());
+        }
+    }
+
+    mod revenue_for_period {
+        use bigdecimal::BigDecimal;
+        use time::OffsetDateTime;
+
+        #[test]
+        fn test_sum() {
+            let mut conn = crate::test::conn();
+            let user_id =
+                *crate::queries::users::create(&mut conn, OffsetDateTime::now_utc()).unwrap();
+            let from = time::macros::datetime!(2023-06-01 00:00).assume_utc();
+            let incoming_at = time::macros::datetime!(2023-06-01 00:01).assume_utc();
+
+            super::create(
+                &mut conn,
+                &super::CreateParams {
+                    user_id,
+                    amount_cents: 69,
+                    description: None,
+                    incoming_at,
+                    created_at: OffsetDateTime::now_utc(),
+                },
+            )
+            .unwrap();
+
+            super::create(
+                &mut conn,
+                &super::CreateParams {
+                    user_id,
+                    amount_cents: 420,
+                    description: None,
+                    incoming_at,
+                    created_at: OffsetDateTime::now_utc(),
+                },
+            )
+            .unwrap();
+
+            let expected = Some(BigDecimal::from(489));
+            let actual =
+                super::revenue_for_period(&mut conn, user_id, from, OffsetDateTime::now_utc())
+                    .unwrap();
+
+            assert_eq!(actual, expected);
         }
     }
 }
